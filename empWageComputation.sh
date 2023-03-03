1b6029d03778177a5fc998f32f2545f669a1e8c3
rishabhcenation <rishabhcenation28@gmail.com>
2023-03-04 02:00:35 +0530

    Refactor: Write a function to get work hours.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,41 +10,6 @@ empRatePerHr=20;
 numWorkingDays=20;
 day=1;
 
-while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
-do
-
-randomCheck=$((RANDOM%3));
-case $randomCheck in $isFullTime)
-                      empHrs=8;;
-                     $isPartTime)
-                      empHrs=4;;
-                     *)
-                      empHrs=0;;
-esac
-totalWorkingHour=$(($totalWorkingHour+$empHrs))
-
-if [ $totalWorkingHour -gt 100 ]
-then
-totalWorkingHour=$(($totalWorkingHour-$empHrs))
-break;
-fi
-salary=$(($empRatePerHr*$empHrs))
-totalSalary=$(($totalSalary+$salary))
-((day++))
-done
-
-echo "Employee has earned $totalSalary $ in a month (Total working Hour : 
-$totalWorkingHour)";
-UC6
-isPartTime=1;
-isFullTime=2;
-empHrs=0;
-totalSalary=0;
-totalWorkingHour=0;
-empRatePerHr=20;
-numWorkingDays=20;
-day=1;
-
 function calculateWorkingHour() {
 	case $randomCheck in 
                      $isFullTime)
